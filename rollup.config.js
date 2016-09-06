1027d428bf6a28bfa2f0b3c3737d34e928820d77
Steve Godin <steve.god@gmail.com>
2016-09-06 11:09:59 -0700

    Install rollup-plugin-includepaths
    Without this plugin, imports have to refer to files beginning for the
    project root path.

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,16 @@
+import includePaths from 'rollup-plugin-includepaths';
+
+const includePathOptions = {
+    include: {},
+    paths: ['src/utils'],
+    external: [],
+    extensions: ['.js', '.json', '.html']
+};
+
 export default {
-  entry: 'src/index.js',
+  entry: './src/index.js',
   format: 'cjs',
-  dest: 'build/index.js'
+  dest: './build/index.js',
+  sourceMap: true,
+  plugins: [ includePaths(includePathOptions) ]
 }
