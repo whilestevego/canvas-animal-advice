6170d759858a1aaea3a1bee1a738b329f0f529b8
Steve Godin <steve.god@gmail.com>
2016-09-16 14:00:53 -0700

    Update compiled assets for Github Pages

--- public/index.js
+++ public/index.js
@@ -155,9 +155,9 @@ const setAttributes = (element, attrs) => {
   }
 }
 
-const createTag = (name, attr = {}) => {
+const createTag = (name, attrs = {}) => {
   const el = document.createElement(name);
-  setAttributes(el, attr);
+  setAttributes(el, attrs);
 
   return el;
 }
@@ -307,6 +307,33 @@ getImage('./imminent-ned.jpg')
 
     writer('');
   })
+
+{
+  const container = findBySelector('#shapes > .canvas-box');
+
+  const canvas = h.canvas({height: 2000, width: 1000});
+  container.appendChild(canvas);
+
+  const π = Math.PI;
+
+  const Artist = cvs => {
+    const ctx = cvs.getContext('2d');
+
+    const circle = ({cx = 0, cy = 0, r = 1}) => {
+      const path = new Path2D();
+
+      path.moveTo(cx, cy);
+      path.arc(cx, cy, r, 0, 2*π, true);
+
+      ctx.fillStyle = 'white';
+      ctx.fill(path);
+    }
+
+    return {circle};
+  };
+
+  Artist(canvas).circle({cx: 100, cy: 100, r: 60});
+};
 
 const extractHash = url => {
   const HASH_REGEX = /#(.+)$/;
