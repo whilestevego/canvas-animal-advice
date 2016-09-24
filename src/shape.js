11a79165dffbb004b3c880ee5220e5ce8ea46c68
Steve Godin <steve.god@gmail.com>
2016-09-24 15:58:54 -0700

    Refactor Artist module

--- src/shape.js
+++ src/shape.js
@@ -1,27 +1,57 @@
 import {findBySelector, findById, h} from 'dom';
+import {each} from 'sequence';
+// TODO: Create Vector class
+// TODO: Create functions to manipulate Vector objects
 {
   const container = findBySelector('#shapes > .canvas-box');
 
   const canvas = h.canvas({height: 2000, width: 1000});
   container.appendChild(canvas);
 
+  // On macOS, press CMD+P for π
   const π = Math.PI;
 
-  const Artist = cvs => {
-    const ctx = cvs.getContext('2d');
+  const circle = (ctx, {cx = 0, cy = 0, radius = 1}) => {
+    const path = new Path2D();
 
-    const circle = ({cx = 0, cy = 0, r = 1}) => {
-      const path = new Path2D();
+    path.arc(cx, cy, radius, 0, 2*π, true);
 
-      path.moveTo(cx, cy);
-      path.arc(cx, cy, r, 0, 2*π, true);
-
-      ctx.fillStyle = 'white';
-      ctx.fill(path);
-    }
-
-    return {circle};
+    ctx.fill(path);
   };
 
-  Artist(canvas).circle({cx: 100, cy: 100, r: 60});
+  const rectangle = (ctx, {cx = 0, cy = 0, width = 2, height = 1}) => {
+    const path = new Path2D();
+
+    path.rect(cx, cy, width, height);
+
+    ctx.fill(path);
+  };
+
+  const square = (ctx, {cx = 0, cy = 0, width = 1}) => {
+    rectangle(ctx, {cx, cy, width, height: width});
+  };
+
+  function Artist (cvs) {
+    const ctx = cvs.getContext('2d');
+    const fns = [circle, square, rectangle];
+
+    ctx.fillStyle = 'White';
+
+    each(
+      fns,
+      fn => {
+        this[fn.name] = arg => {
+          arg ? fn(ctx, arg) : fn(ctx, {});
+          return this;
+        };
+      }
+    );
+  };
+
+  const artist = new Artist(canvas);
+
+  artist
+    .circle({cx: 100, cy: 100, radius: 60})
+    .circle({cx: 200, cy: 300, radius: 80})
+    .square({cx: 400, cy: 200, width: 60 * 3});
 };
